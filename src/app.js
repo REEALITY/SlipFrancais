1b4bc0eb757f7a970739e3d9e481604718c04e27
Clement Michaud <clement.michaud34@gmail.com>
2017-03-18 03:16:46 +0100

    Handle the cart and listen to click on add_to_cart button

---
+++ src/app.js
@@ -0,0 +1,19 @@
+
+var articles = require('./articles');
+var Cart = require('./cart');
+var css_classes = require('./css_classes');
+
+
+module.exports = function(deps) {
+  var $ = deps.jQuery;
+  var cart = new Cart(); 
+
+  var add_to_cart_button = $(css_classes.add_to_cart);
+  add_to_cart_button.on('click', function() {
+    display_cart($); 
+  });
+}
+
+function display_cart($) {
+  $(css_classes.cart_board).show();
+}
