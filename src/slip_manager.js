405015b461542a33bcfc9eaddc7f7a8167291d56
sergei <serge.kireev.fr@gmail.com>
2017-03-18 13:15:18 +0100

    added fiche produit

--- src/slip_manager.js
+++ src/slip_manager.js
@@ -7,7 +7,6 @@ function displaySelected($, id) {
     var slip_items_bkg = $(css_classes.slip_item_bkg);
     slip_items_bkg.each(function(i) {
         var bkg_id = $(this).attr('id'); 
-        console.log(bkg_id, id+BKG_SUFFIX);
         if (bkg_id == id+BKG_SUFFIX) {
             $( this ).attr("scale", SCALE+" "+SCALE+" "+SCALE);
         }
@@ -17,11 +16,19 @@ function displaySelected($, id) {
     });
 }
 
+function showFicheProduit($)
+{
+    var fiche_produit = $(css_classes.fiche_produit);
+    console.log(fiche_produit.attr("id"));
+    fiche_produit.attr("visible", "true");
+}
+
 module.exports.attachClickListener = function($, selected_slip) {
     var slip_items = $(css_classes.slip_item);
     slip_items.on('click', function(e) {
         var id = $(this).attr('id');
         selected_slip.slip = id;
         displaySelected($, id);
+        showFicheProduit($);
     });
 };
